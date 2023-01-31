26fffdab48dc3e2ba4df9281695c1655e27ffc76
maicon.silva <maicon.silva@ganex.com.br>
2023-01-31 17:31:52 -0300

    updated examples

--- modules/web-acl/examples/complete/main.tf
+++ modules/web-acl/examples/complete/main.tf
@@ -11,64 +11,24 @@ module "waf_web_acl_test" {
     }
   ]
 
- managed_rules = [
+  managed_rules = [
     {
-      name            = "AWSManagedRulesCommonRuleSet",
-      priority        = 1
-      override_action = "none"
-      rule_action_override  = [
-        {
-          name = "CrossSiteScripting_BODY"
-          action = "count"
-        },
-        {
-          name = "EC2MetaDataSSRF_BODY"
-          action = "count"
-        },
-        {
-          name = "SizeRestrictions_BODY"
-          action = "allow"
-        } 
-      ]
-    },
-    {
-      name            = "AWSManagedRulesSQLiRuleSet",
-      priority        = 2
-      override_action = "none"
-      rule_action_override  = []
-    },
-    {
-      name            = "AWSManagedRulesPHPRuleSet",
-      priority        = 3
-      override_action = "none"
-      rule_action_override  = []
-    },
-    {
-      name            = "AWSManagedRulesWordPressRuleSet",
-      priority        = 4
+      name            = "AWSManagedRulesLinuxRuleSet",
+      priority        = 50
       override_action = "none"
       rule_action_override  = []
     },
     {
       name            = "AWSManagedRulesAmazonIpReputationList",
-      priority        = 5
-      override_action = "none"
+      priority        = 2
+      override_action = "count"
       rule_action_override  = []
     },
     {
       name            = "AWSManagedRulesBotControlRuleSet",
-      priority        = 6
-      override_action = "none"
-      rule_action_override  = [
-        {
-          name = "CategoryHttpLibrary"
-          action = "count"
-        },
-        {
-          name = "SignalNonBrowserUserAgent"
-          action = "count"
-        }
-      ]
+      priority        = 1
+      override_action = "count"
+      rule_action_override  = []
     },
   ]
 }
